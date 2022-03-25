63002adadbc3bf163dbc2de79c40a28b66fbdfde
Caswell Orr <caswell.orr@gmail.com>
2022-03-25 11:38:17 -0500

    boiler components

--- src/App.js
+++ src/App.js
@@ -12,22 +12,7 @@ function App() {
 
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/">
-            <Route index element={<Home />} />
-          </Route>
-          <Route exact path="/bio">
-            <Route index element={<Bio />} />
-          </Route>
-          <Route exact path="/gallery">
-            <Route index element={<Gallery />} />
-          </Route>
-          <Route exact path="/subscribe">
-            <Route index element={<Subscribe />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/components/work/Work.js
@@ -0,0 +1,9 @@
+import './work.scss'
+
+function Work() {
+  return (
+    <div className='work'>Work</div>
+  )
+}
+
+export default Work
