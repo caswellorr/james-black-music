1a039ba5682359c14f5259485c7cdb3bee62ca98
Caswell Orr <caswell.orr@gmail.com>
2022-04-01 13:58:20 -0500

    mailchimp is a pain in my ass

--- client/src/components/form/Form.js
+++ client/src/components/form/Form.js
@@ -5,8 +5,9 @@ import React, { useState, useEffect } from 'react';
 function Form() {
 
   const intialState = {
-    name: '',
-    email: ''
+    // name: '',
+    // email: '',
+    MERGE0: '',
   };
 
   const [ formValues, setFormValues] = useState(intialState);
@@ -38,17 +39,17 @@ function Form() {
     const errors = {};
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
-    if(!values.email) {
-      errors.email = "Email is required!"
-    } else if (!regex.test(values.email)) {
-      errors.email = "Please enter a valid email."
+    if(!values.MERGE0) {
+      errors.MERGE0 = "Email is required!"
+    } else if (!regex.test(values.MERGE0)) {
+      errors.MERGE0 = "Please enter a valid email."
     }
     
-    if(!values.name) {
-      errors.name = "Name is required!"
-    } else if (!regex.test(values.name)) {
-      errors.name = "Please enter your name."
-    }
+    // if(!values.name) {
+    //   errors.name = "Name is required!"
+    // } else if (!regex.test(values.name)) {
+    //   errors.name = "Please enter your name."
+    // }
 
     return errors;
 
@@ -59,7 +60,11 @@ function Form() {
          <h2>subscribe</h2>
        </div>
        <form 
-         onSubmit={handleSubmit}>
+         onSubmit={handleSubmit}
+         action="https://gmail.us14.list-manage.com/subscribe/post" method="POST"
+         >
+        <input type="hidden" name="u" value="970d2e87e09f618fb90406232" />
+        <input type="hidden" name="id" value="2299ada69a" />
          <div className='inputs-section'>
            {/* <label className="subscribeInput animation">
              <input 
@@ -72,16 +77,19 @@ function Form() {
              <span className="placeholder">name</span>
              <p className='form-error'>{ formErrors.name }</p>
            </label> */}
-           <label className="subscribeInput animation">
+           <label className="subscribeInput animation"
+           htmlFor='MERGE0'
+           >
              <input 
-              
-               type="text"
-               name="email"
-               value={ formValues.email }
-               onChange={handleChange}
-               placeholder="&nbsp;"/>
+                name="MERGE0" 
+                id="MERGE0"
+                type="email"
+                // name="email"
+                value={ formValues.MERGE0 }
+                onChange={handleChange}
+                placeholder="&nbsp;"/>
              <span className="placeholder">email address</span>
-             <p className='form-error'>{ formErrors.email }</p>
+             <p className='form-error'>{ formErrors.MERGE0 }</p>
            </label>
          </div>
          <div className='subscribe-button'>
@@ -89,6 +97,8 @@ function Form() {
              submit
            </button>
          </div>
+         <input type="hidden" name="ht" value="27cf3ffe574fabec239aeccf55d38a55e646a4ec:MTY0ODgzNzg2MS4xNjk=" />
+        <input type="hidden" name="mc_signupsource" value="hosted" />
        </form>
       </div>
   )
